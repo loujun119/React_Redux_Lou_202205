0a8d2add1121782112c4ac550560d653fae5d952
loujun <loujun8086@yahoo.co.jp>
2022-05-05 23:55:53 +0900

    router

---
+++ src/components/LoginIn.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function LoginIn() {
+  return <div>
+      <h1>Login In Page</h1>
+      <table>
+          <thead>
+              <tr>name</tr>
+              <tr>age</tr>
+              <tr>birthday</tr>
+          </thead>
+      </table>
+  </div>;
+}
--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,10 +1,22 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AppFooter from "../components/AppFooter";
+import AppHeader from "../components/AppHeader";
+import Home from "../components/Home";
+import LoginIn from "../components/LoginIn";
 
-function App() {
-  return (
-    <div className="App">
-      <h1> hello react world</h1>
-    </div>
-  );
-}
+// const isDevelopEnv: boolean = process.env.NODE_ENV === "development";
+const App: React.FC = () => (
+  <>
+    <AppHeader />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<LoginIn />} />
+      </Routes>
+    </Router>
+    <AppFooter />
+  </>
+);
 
 export default App;
