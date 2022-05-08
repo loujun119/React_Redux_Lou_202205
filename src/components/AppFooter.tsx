38fe64f1bf4adb6b90cc653eb88b533e6c841cb3
loujun119 <loujun8086@yahoo.co.jp>
2022-05-08 18:17:26 +0900

    1

--- src/components/AppFooter.tsx
+++ src/components/AppFooter.tsx
@@ -13,5 +13,5 @@ const AppFooterStyles = makeStyles({
 
 export default function AppFooter() {
   const classes = AppFooterStyles();
-  return <div className={classes.footer}> AppFooter </div>;
+  return <div className={classes.footer}> AppFooter1 </div>;
 }
